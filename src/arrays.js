3e16fe6acc4285e358881b87827e0f2b361842ca
Tyler Olsen <tylerkolsen@gmail.com>
2024-08-02 11:07:57 -0600

    Completed first two array questions

--- src/arrays.js
+++ src/arrays.js
@@ -2,13 +2,29 @@
 // Ex.:
 //   greaterThanTen([1, 2, 3, 11, 12, 13]);
 //   => [11, 12, 13]
-function greaterThanTen(numbers) {}
+function greaterThanTen(numbers) {
+  let greaterArray = []
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > 10) {
+      greaterArray.push(numbers[i])
+    }
+  }
+  return greaterArray
+}
 
 // Given an array of strings, return all words that start with 'b' or 'B'.
 // Ex.:
 //   bWords(['banana', 'orange', 'apple', 'Bonobo', 'kiwi', 'pear']);
 //   => ['banana', 'Bonobo]
-function bWords(words) {}
+function bWords(words) {
+  let goodWordsArray = []
+  for (let i = 0; i < words.length; i++) {
+    if (words[i][0] === "b" || words[i][0] === "B") {
+      goodWordsArray.push(words[i])
+    }
+  }
+  return goodWordsArray
+}
 
 // Add all the elements from additionalItems to the end of originalArray.
 // Return the originalArray..
