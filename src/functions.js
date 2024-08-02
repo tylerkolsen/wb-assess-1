f2177dd13f6d5a3f30675c3625aa9147d9335981
Tyler Olsen <tylerkolsen@gmail.com>
2024-08-02 12:38:06 -0600

    Finished first 4 problems in functions

--- src/functions.js
+++ src/functions.js
@@ -2,7 +2,9 @@
 // Ex.:
 //   divide(10, 2);
 //   => 5
-function divide(x, y) {}
+function divide(x, y) {
+  return x / y
+}
 
 // Return the average of x and y.
 // Here, the average of x and y is the *mean* of x and y. In other words, it's
@@ -10,7 +12,9 @@ function divide(x, y) {}
 // Ex.:
 //   average(10, 2);
 //   => 6
-function average(x, y) {}
+function average(x, y) {
+  return (x + y) / 2
+}
 
 // Return true if x and y are mostly equivalent up to a tolerance of 0.001.
 // In other words, return true if the aboslute value of x - y is less than 0.001.
@@ -18,13 +22,21 @@ function average(x, y) {}
 // Ex.:
 //   approximatelyEqual(10.001, 10);
 //   => true
-function approximatelyEqual(x, y) {}
+function approximatelyEqual(x, y) {
+  if (Math.abs(x-y) < 0.001) {
+    return true
+  } else {
+    return false
+  }
+}
 
 // Given a first name and last name, return a full name in the format "FIRST LAST"
 // Ex.:
 //   fullName('John', 'Doe');
 //   => 'John Doe'
-function fullName(firstName, lastName) {}
+function fullName(firstName, lastName) {
+  return `${firstName} ${lastName}`
+}
 
 // Generate the sentence "PERSON was drinking BEVERAGE at LOCATION" using the
 // person, beverage and location provided.
