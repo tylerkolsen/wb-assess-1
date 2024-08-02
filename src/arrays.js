0a7a7d58df70833516fa90fc527c1b9a2d179c3e
Tyler Olsen <tylerkolsen@gmail.com>
2024-08-02 12:08:52 -0600

    finished all array problems

--- src/arrays.js
+++ src/arrays.js
@@ -86,7 +86,22 @@ function findWordsStartingWith(words, letter) {
 // Ex.:
 //   smallestNItems([1, 30, 4, 21, 100000], 3);
 //   => [21, 4, 1]
-function smallestNItems(items, n) {}
+function smallestNItems(items, n) {
+  items.sort(function(a, b){return b-a})
+  console.log(items)
+  const smallestArray = []
+  for (let i=(items.length - n); i < items.length; i++) {
+    smallestArray.push(items[i])
+  }
+  return smallestArray
+}
+// pseudocode idea is to create an array with the numbers placed in descending order, and then .pop the end using n for the amount to pop.
+// I looked up the .sort method. Unfortunately it sorts by strings, but there was another site that offered the Compare function to allow a mathematical comparison
+// array.sort(function(a, b){return a-b}); 
+// This one does it in ascending order, so for descending order
+// array.sort(function(a, b){return b-a});
+// After some struggle, i don't think pop will work. I'm going to do ascending order on the array, then push up to n times
+// I also realized it needs to be descending order :). I can put it back in descending order, and then go (array.length - n) to start
 
 // Search for a value in the array and return its index. If the value appears
 // more than once, return the index of the *FIRST* occurrence of the value. If
@@ -94,14 +109,28 @@ function smallestNItems(items, n) {}
 // Ex.:
 //   findIndex(['a', 'b', 'c', 'a', 'b', 'c'], 'c');
 //   => 2
-function findIndex(items, value) {}
+function findIndex(items, v) {
+  console.log(items)
+  for (let i = 0; i < items.length; i++) {
+    if (items[i] === v) {
+      console.log(i)
+      return items.indexOf(v)
+    }
+  }
+}
 
 // Given a start number and stop number, return a new array containing all the numbers
 // between the start and stop number.
 // Ex.:
 //   range(1, 5);
 //   => [1, 2, 3, 4, 5]
-function range(start, stop) {}
+function range(start, stop) {
+  let numbersArray = []
+  for (let i = start; i <= stop; i++) {
+    numbersArray.push(i)
+  }
+  return numbersArray
+}
 
 export {
   bWords,
